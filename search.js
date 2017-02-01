27f699936c50917c78d551dc76e8213498d42489
aliciabigley <aliciabigley@gmail.com>
2017-02-01 14:52:08 -0600

    ajax added, reworked functions

--- search.js
+++ search.js
@@ -1,35 +1,47 @@
-function ajax(){
-  $.ajax({
-    url: url,
-    method: 'get',
-    data; {term:requestData},
-    dataType; 'json',
-    success: function(data){
-      if(){
-
-      }
-      else{}
-    }
-  });
-
-}
-
-
-
-function searchMusic(){
-var searchItunes = combineInputAndApi();
-return searchItunes
-}
-
-function getInput(){
-  var userInput = $("#userInput").val();
-  return userInput;
-}
-
+// $('#search').click(function(){ // can i call this from html?
+//   var itunesApi = "https://itunes.apple.com/search?term=";
+//   var userInput = getserInput()
+//   var url = itunesApi + userInput
+//   $.ajax({
+//     url: url,
+//     method: 'get',
+//     data: {term: userInput},
+//     dataType: 'jsonp',
+//   });
+// });
 
 function startSearch(){
   var itunesApi = "https://itunes.apple.com/search?term=";
-  var input = getInput();
-  var searchFor = itunesApi + input;
-  return searchFor;
+  var userInput = getserInput()
+  var url = itunesApi + userInput
+  $.ajax({
+    url: url,
+    method: 'get',
+    data: {term: userInput},
+    dataType: 'jsonp'
+  });
+
 }
+function getserInput(){
+  var getInput = $("#userInput").val();
+  return getInput;
+}
+
+
+// function searchMusic(){
+// var searchItunes = combineInputAndApi();
+// return searchItunes
+// }
+//
+// function userInput(){
+//   var getInput = $("#userInput").val();
+//   return getInput;
+// }
+
+//
+// function startSearch(){
+//
+//   var input = getInput();
+//   var searchFor = itunesApi + input;
+//   return searchFor;
+// }
